8fc0560c68355d44c40432a1f2b35ec6a9127d9d
전현민 <joyjhm@naver.com>
2025-12-30 14:45:55 +0900

    fix: thread 무한 생성 변경
    - ExecutorService 사용하여 Concurrent로 사용하도록 수정

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +21,11 @@ public class WebServer {
             port = Integer.parseInt(args[0]);
         }
 
+        ExecutorService pool = Executors.newFixedThreadPool(100);
+        RequestParser requestParser = new RequestParser();
+        StaticResourceHandler staticResourceHandler = new StaticResourceHandler();
+        Router router = new Router(staticResourceHandler);
+
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
@@ -27,8 +33,7 @@ public class WebServer {
             // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection, new RequestParser(), new Router(new StaticResourceHandler())));
-                thread.start();
+                pool.submit(new RequestHandler(connection, requestParser, router));
             }
         }
     }
