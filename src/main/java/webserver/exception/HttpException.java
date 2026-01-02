723f1912a1147f02e722477f76cda4354d1562d4
전현민 <joyjhm@naver.com>
2026-01-02 23:10:12 +0900

    feat: HttpException 추상클래스 구현

---
+++ src/main/java/webserver/exception/HttpException.java
@@ -0,0 +1,18 @@
+package webserver.exception;
+
+import webserver.response.HttpStatus;
+
+public abstract class HttpException extends RuntimeException {
+
+    private final HttpStatus HttpStatus;
+
+    public HttpException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.HttpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return HttpStatus;
+    }
+}
+
