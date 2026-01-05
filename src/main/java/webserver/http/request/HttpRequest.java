1305725f662b932adecdb854cfce837fcaa6d9a0
전현민 <joyjhm@naver.com>
2026-01-05 17:37:54 +0900

    refactor: 패키지 이동 및 convertor에서 attributes 저장 방식 개선
    - attributes 멤버 변수 추가
    - getAtrribute, setAttribute 추가
    - http 패키지 추가 후 파일 이동

---
+++ src/main/java/webserver/http/request/HttpRequest.java
@@ -0,0 +1,51 @@
+package webserver.http.request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+
+    private final HttpRequestStartLine startLine;
+    private final Map<String, String> headers;
+    private final String body;
+    private final Map<String, String> attributes = new HashMap<>();
+
+    HttpRequest(HttpRequestStartLine startLine, Map<String, String> headers, String body) {
+        this.startLine = startLine;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public String getPath() {
+        return startLine.getPath();
+    }
+
+    public String getParam(String parameterName) {
+        return startLine.getParam(parameterName);
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public String getHeader(String key) {
+        return headers.get(key);
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public HttpMethod getMethod() {
+        return startLine.getMethod();
+    }
+
+    public void setAttribute(String name, String value) {
+        attributes.put(name, value);
+    }
+
+    public String getAttribute(String attributeName) {
+        return attributes.get(attributeName);
+    }
+
+}
