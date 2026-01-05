325bd8c845251d8d59efb2ca5ef8cb052f9437c6
전현민 <joyjhm@naver.com>
2026-01-05 17:42:15 +0900

    refactor: HttpExceptionHandler 도입에 따라 Dispatcher 예외 위임

--- src/main/java/webserver/HttpRequestDispatcher.java
+++ src/main/java/webserver/HttpRequestDispatcher.java
@@ -1,35 +1,40 @@
 package webserver;
 
-import webserver.request.HttpRequest;
-import webserver.response.HttpResponse;
-import java.io.*;
+import webserver.exception.HttpException;
+import webserver.exception.HttpExceptionHandler;
+import webserver.http.request.HttpRequest;
+import webserver.http.response.HttpResponse;
 
 public class HttpRequestDispatcher {
 
     private final StaticResourceHandler staticResourceHandler;
     private final DynamicResourceHandler dynamicResourceHandler;
+    private final HttpExceptionHandler httpExceptionHandler;
 
-    public HttpRequestDispatcher(StaticResourceHandler staticResourceHandler, DynamicResourceHandler dynamicResourceHandler) {
+    public HttpRequestDispatcher(StaticResourceHandler staticResourceHandler, DynamicResourceHandler dynamicResourceHandler, HttpExceptionHandler httpExceptionHandler) {
         this.staticResourceHandler = staticResourceHandler;
         this.dynamicResourceHandler = dynamicResourceHandler;
+        this.httpExceptionHandler = httpExceptionHandler;
     }
 
-    public HttpResponse dispatch(HttpRequest httpRequest) throws IOException {
-        String path = httpRequest.getStartLine().getRequestURL().getPath();
+    public HttpResponse dispatch(HttpRequest httpRequest) {
+        String path = httpRequest.getPath();
         boolean isStatic = path.endsWith(".html")
                 || path.endsWith(".css")
                 || path.endsWith(".js")
                 || path.endsWith(".ico")
                 || path.endsWith(".png")
                 || path.endsWith(".jpg")
-                || path.endsWith(".svg")
-                || path.equals("/");
+                || path.endsWith(".svg");
 
-        if(isStatic) {
-            return staticResourceHandler.handle(httpRequest);
+        try {
+            if(isStatic) {
+                return staticResourceHandler.handle(httpRequest);
+            }
+            return dynamicResourceHandler.handle(httpRequest);
+        } catch (HttpException e) {
+            return httpExceptionHandler.handle(httpRequest, e);
         }
-
-        return dynamicResourceHandler.handle(httpRequest);
 
     }
 
