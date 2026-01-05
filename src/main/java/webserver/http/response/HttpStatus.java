fca74039edea625bcd35e80ade8ad1937864b5c7
전현민 <joyjhm@naver.com>
2026-01-05 17:43:16 +0900

    refactor: HttpStatus 상태 코드 확장 및 파일 위치 정리

--- src/main/java/webserver/http/response/HttpStatus.java
+++ src/main/java/webserver/http/response/HttpStatus.java
@@ -1,8 +1,11 @@
-package webserver.response;
+package webserver.http.response;
 
 public enum HttpStatus {
     OK(200),
     CREATED(201),
+    FOUND(302),
+    BAD_REQUEST(400),
+    UNAUTHORIZED(401),
     NOT_FOUND(404),
     INTERNAL_SERVER_ERROR(500);
 
