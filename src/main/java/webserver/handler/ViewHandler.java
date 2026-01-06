c301c718c7712647de6cf2cd92557681b2b6fd44
전현민 <joyjhm@naver.com>
2026-01-06 18:46:22 +0900

    refactor: ViewHandler Model 인자 추가
    - html 동적 처리를 위한 Model 객체 추가

--- src/main/java/webserver/handler/ViewHandler.java
+++ src/main/java/webserver/handler/ViewHandler.java
@@ -1,8 +1,9 @@
 package webserver.handler;
 
+import webserver.Model;
 import webserver.http.request.HttpRequest;
 
 public interface ViewHandler {
 
-    String handle(HttpRequest request);
+    String handle(HttpRequest request, Model model);
 }
