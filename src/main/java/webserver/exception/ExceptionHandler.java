dd32ccdd835ab436f25a4ea06afdba14525885e2
전현민 <joyjhm@naver.com>
2026-01-07 17:56:18 +0900

    feat: HttpException 이외의 Exception 처리하는 GlobalHandle 구현

--- src/main/java/webserver/exception/ExceptionHandler.java
+++ src/main/java/webserver/exception/ExceptionHandler.java
@@ -5,13 +5,14 @@ import webserver.http.request.HttpRequest;
 import webserver.http.response.ContentType;
 import webserver.http.response.HttpResponse;
 import webserver.http.response.HttpResponseBuilder;
+import webserver.http.response.HttpStatus;
 
 import java.nio.charset.StandardCharsets;
 
 
-public class HttpExceptionHandler {
+public class ExceptionHandler {
 
-    public HttpResponse handle(HttpRequest request, HttpException e) {
+    public HttpResponse httpExceptionHandle(HttpRequest request, HttpException e) {
         String json = String.format(
                 """
                 {
@@ -25,6 +26,26 @@ public class HttpExceptionHandler {
         byte[] body = json.getBytes(StandardCharsets.UTF_8);
 
         return new HttpResponseBuilder().statusLine(e.getHttpStatus())
+                .header(HttpHeader.CONTENT_TYPE, ContentType.JSON.getMimeType())
+                .header(HttpHeader.Content_LENGTH, String.valueOf(body.length))
+                .body(json)
+                .build();
+    }
+
+    public HttpResponse globalExceptionHandle(HttpRequest request, Exception e) {
+        String json = String.format(
+                """
+                {
+                  "path": "%s",
+                  "message": "%s"
+                }
+                """,
+                request.getPath(),
+                e.getMessage()
+        );
+        byte[] body = json.getBytes(StandardCharsets.UTF_8);
+
+        return new HttpResponseBuilder().statusLine(HttpStatus.INTERNAL_SERVER_ERROR)
                 .header(HttpHeader.CONTENT_TYPE, ContentType.JSON.getMimeType())
                 .header(HttpHeader.Content_LENGTH, String.valueOf(body.length))
                 .body(json)
