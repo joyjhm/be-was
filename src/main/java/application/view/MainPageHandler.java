6f85d0e2bf48298251b49f0f62b441428e49d1f8
전현민 <joyjhm@naver.com>
2026-01-07 17:57:48 +0900

    fix: main/index.html 받아서 데이터 주입하도록 수정

--- src/main/java/application/view/MainPageHandler.java
+++ src/main/java/application/view/MainPageHandler.java
@@ -7,44 +7,23 @@ import webserver.http.request.HttpRequest;
 
 public class MainPageHandler implements ViewHandler {
 
-    private static final String MAIN_PAGE_RESOURCE_PATH = "/index.html";
+    private static final String MAIN_PAGE_PATH = "/index.html";
+    private static final String LOGGED_IN_MAIN_PAGE_PATH = "/main/index.html";
 
     @Override
     public String handle(HttpRequest request, Model model) {
-        HttpSession httpSession = request.getSession();
+        HttpSession session = request.getSession();
 
-        //TODO: 현재 테스트를 위해 html 코드 전체 변경, 추후에 수정 예정
-        String name = (String) httpSession.getAttribute("name");
-
-        String html;
-        if(name == null) {
-            html =
-                    """
-                                      <li class="header__menu__item">
-                                        <a class="btn btn_contained btn_size_s" href="/login">로그인</a>
-                                      </li>
-                                      <li class="header__menu__item">
-                                        <a class="btn btn_ghost btn_size_s" href="/registration">
-                                          회원 가입
-                                        </a>
-                                      </li>
-                            
-                            """;
-        } else {
-            html = String.format("""
-                          <li class="header__menu__item">
-                            <form method="POST" action="/logout">
-                              <button class="btn btn_contained btn_size_s" type="submit">로그아웃</button>
-                            </form>
-                          </li>
-                          <li class="header__menu__item">
-                            <a class="btn btn_ghost btn_size_s" href="/mypage">%s</a>
-                          </li>
-                """, name);
+        if (session == null) {
+            return MAIN_PAGE_PATH;
         }
 
-        model.setAttribute("header", html);
+        String name = (String) session.getAttribute("name");
+        if (name == null) {
+            return MAIN_PAGE_PATH;
+        }
 
-        return MAIN_PAGE_RESOURCE_PATH;
+        model.setAttribute("name", name);
+        return LOGGED_IN_MAIN_PAGE_PATH;
     }
 }
