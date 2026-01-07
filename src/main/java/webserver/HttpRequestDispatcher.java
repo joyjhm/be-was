3e43a5556ea8fc43ce00d5ddf2d1052f4c680ebe
전현민 <joyjhm@naver.com>
2026-01-07 17:56:44 +0900

    refactor: Converter, Resolver 처리 Dispatcher로 이동

--- src/main/java/webserver/HttpRequestDispatcher.java
+++ src/main/java/webserver/HttpRequestDispatcher.java
@@ -1,20 +1,34 @@
 package webserver;
 
+import webserver.convertor.ConvertorRegistry;
+import webserver.convertor.HttpMessageConvertor;
 import webserver.exception.HttpException;
-import webserver.exception.HttpExceptionHandler;
+import webserver.exception.ExceptionHandler;
 import webserver.http.request.HttpRequest;
 import webserver.http.response.HttpResponse;
+import webserver.resolver.HttpResolver;
+import webserver.resolver.ResolverRegistry;
 
 public class HttpRequestDispatcher {
 
     private final StaticResourceHandler staticResourceHandler;
     private final DynamicResourceHandler dynamicResourceHandler;
-    private final HttpExceptionHandler httpExceptionHandler;
+    private final ExceptionHandler exceptionHandler;
+    private final ConvertorRegistry convertorRegistry;
+    private final ResolverRegistry resolverRegistry;
 
-    public HttpRequestDispatcher(StaticResourceHandler staticResourceHandler, DynamicResourceHandler dynamicResourceHandler, HttpExceptionHandler httpExceptionHandler) {
+    public HttpRequestDispatcher(
+            StaticResourceHandler staticResourceHandler,
+            DynamicResourceHandler dynamicResourceHandler,
+            ExceptionHandler exceptionHandler,
+            ResolverRegistry resolverRegistry,
+            ConvertorRegistry convertorRegistry) {
         this.staticResourceHandler = staticResourceHandler;
         this.dynamicResourceHandler = dynamicResourceHandler;
-        this.httpExceptionHandler = httpExceptionHandler;
+        this.exceptionHandler = exceptionHandler;
+        this.resolverRegistry = resolverRegistry;
+        this.convertorRegistry = convertorRegistry;
+
     }
 
     public HttpResponse dispatch(HttpRequest httpRequest) {
@@ -28,12 +42,24 @@ public class HttpRequestDispatcher {
                 || path.endsWith(".svg");
 
         try {
+            for (HttpResolver httpResolver: resolverRegistry.getHttpResolvers()) {
+                httpResolver.resolve(httpRequest);
+            }
+
+            for (HttpMessageConvertor<HttpRequest> messageConvertor: convertorRegistry.getHttpMessageRequestConverters()) {
+                if (messageConvertor.canConvert(httpRequest)) {
+                    messageConvertor.convert(httpRequest);
+                }
+            }
+
             if(isStatic) {
                 return staticResourceHandler.handle(httpRequest);
             }
             return dynamicResourceHandler.handle(httpRequest);
         } catch (HttpException e) {
-            return httpExceptionHandler.handle(httpRequest, e);
+            return exceptionHandler.httpExceptionHandle(httpRequest, e);
+        } catch (Exception e) {
+            return exceptionHandler.globalExceptionHandle(httpRequest, e);
         }
 
     }
