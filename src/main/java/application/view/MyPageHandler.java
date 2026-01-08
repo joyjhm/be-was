78eb56263906ffca5aa89de6b32c8a19dc8546f9
전현민 <joyjhm@naver.com>
2026-01-08 18:37:51 +0900

    fix: 마이페이지 로그인 상태 아닐 시 로그인 페이지로 리다이렉션

--- src/main/java/application/view/MyPageHandler.java
+++ src/main/java/application/view/MyPageHandler.java
@@ -7,9 +7,14 @@ import webserver.http.request.HttpRequest;
 public class MyPageHandler implements ViewHandler {
 
     private static final String MY_PAGE_RESOURCE_PATH = "/mypage/index.html";
+    private static final String LOGIN_PAGE = "/login/index.html";
 
     @Override
     public String handle(HttpRequest request, Model model) {
+        if (request.getSession() == null || request.getSession().getAttribute("userId") == null) {
+            return LOGIN_PAGE;
+        }
+
         return MY_PAGE_RESOURCE_PATH;
     }
 }
