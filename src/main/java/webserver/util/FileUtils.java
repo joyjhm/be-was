dfeb6a6ebd0a05de14b161204309af0dea0fd1fd
전현민 <joyjhm@naver.com>
2026-01-15 02:08:34 +0900

    feat: file 쓰기 기능 추가

--- src/main/java/webserver/util/FileUtils.java
+++ src/main/java/webserver/util/FileUtils.java
@@ -3,11 +3,23 @@ package webserver.util;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.exception.InternalServerException;
 
 public class FileUtils {
+    private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
 
     static public byte[] readFile(String filePath) throws IOException {
-        try (FileInputStream fis = new FileInputStream(filePath);
+        String decodedPath =
+                java.net.URLDecoder.decode(filePath, StandardCharsets.UTF_8);
+
+        logger.info("Trying to read resource from file: {}", decodedPath);
+
+        try (FileInputStream fis = new FileInputStream(decodedPath);
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
             byte[] buffer = new byte[4096];
@@ -18,5 +30,14 @@ public class FileUtils {
             }
             return baos.toByteArray();
         }
+    }
+
+    static public void writeFile(String filePath, byte[] data) {
+        try {
+            Files.write(Path.of(filePath), data);
+        } catch (IOException e) {
+            throw new InternalServerException(e.getMessage());
+        }
+
     }
 }
